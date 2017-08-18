f8b2370238b069bdfe319f037a20b2ba8ca498ca
Mark Bates <mark@markbates.com>
2017-08-18 14:12:50 +0100

    added a couple of simple tests

--- actions/packages_test.go
+++ actions/packages_test.go
@@ -5,6 +5,11 @@ import (
 	"github.com/bketelsen/rocksweb/testdata"
 )
 
+func (a *ActionSuite) Test_PackageIndex() {
+	res := a.HTML("/").Get()
+	a.Equal(200, res.Code)
+}
+
 func (a *ActionSuite) Test_PackagesSearch() {
 	err := testdata.CreateBuffalo(a.DB)
 	a.NoError(err)
@@ -16,4 +21,19 @@ func (a *ActionSuite) Test_PackagesSearch() {
 	pkgs := models.Packages{}
 	res.Bind(&pkgs)
 	a.Len(pkgs, 1)
+}
+
+func (a *ActionSuite) Test_PackagesSearch_NoArgs() {
+	err := testdata.CreateBuffalo(a.DB)
+	a.NoError(err)
+
+	err = testdata.CreateVice(a.DB)
+
+	psq := models.PackageSearchQuery{}
+	res := a.JSON("/packages/search").Post(&psq)
+	a.Equal(200, res.Code)
+
+	pkgs := models.Packages{}
+	res.Bind(&pkgs)
+	a.Len(pkgs, 2)
 }
